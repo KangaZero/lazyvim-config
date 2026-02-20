b05233d2420eb1da38a3bd5ddac922452b9f69e9
samuelwaiweng.yong <samuelyongw@gmail.com>
2026-02-20 18:13:21 +0900

    Removed neo-tree plugin, edited ui & todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,6 +2,10 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    pattern = [[\b(KEYWORDS)(:|\s)]],
+    keywords = {
+      SPECIAL = { icon = "特", color = "#3AE0BB" },
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
